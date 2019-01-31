c45ed07740ae264454ee7a05a5dc0dc4736bfcaa
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-01-31 11:58:57 +0100

    Add files via upload

---
+++ Random case.js
@@ -0,0 +1,33 @@
+/*
+Write a function that will randomly upper and lower characters in a string - randomCase() (random_case() for Python).
+
+A few examples:
+
+randomCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit") == "lOReM ipSum DOloR SiT AmeT, cOnsEcTEtuR aDiPiSciNG eLIt"
+
+randomCase("Donec eleifend cursus lobortis") == "DONeC ElEifEnD CuRsuS LoBoRTIs"
+Note: this function will work within the basic ASCII character set to make this kata easier - so no need to make the function multibyte safe.
+
+
+ */
+//My solution:
+function randomCase(x) {
+    let res='';
+    for(let i=0; i<x.length; i++){
+        if (Math.random() > 0.5){
+            res += x[i].toUpperCase();
+        }else{
+            res += x[i].toLowerCase();
+        }
+    }
+    return res;
+}
+/*
+or:
+function randomCase(x) {
+  return x.split('')
+          .map(function(e) { return Math.random() < 0.5 ? e.toUpperCase() : e.toLowerCase(); })
+          .join('');
+}
+
+ */
