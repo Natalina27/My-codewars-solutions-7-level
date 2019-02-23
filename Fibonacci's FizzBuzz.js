af55773a267f08783f3970768eb1c8c163058602
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-02-23 11:06:09 +0100

    Update Fibonacci's FizzBuzz.js

--- Fibonacci's FizzBuzz.js
+++ Fibonacci's FizzBuzz.js
@@ -36,6 +36,7 @@ Output:
  */
 
 //My solutions:
+//1.
 const fibsFizzBuzz = function(n) {
     if (n===1){
         return [1];
@@ -62,4 +63,15 @@ const fibsFizzBuzz = function(n) {
     }
     return arr2;
 }
+//2.
+function fibsFizzBuzz(n) {
+  let res = [], [a, b] = [0, 1];
+  for (let i = 0; i < n; i++) {
+    [a, b] = [b, a + b];
+    res.push(a % 15 == 0 ? 'FizzBuzz' :
+      a % 3 == 0 ? 'Fizz' : a % 5 == 0 ? 'Buzz' : a);
+  }
+  return res;
+}
+
  
