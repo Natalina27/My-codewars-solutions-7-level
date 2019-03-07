6e77be23a5b0d47e8f97a70a81944dd4268c86b7
Natalya Myunster <35192320+Natalina27@users.noreply.github.com>
2019-03-07 11:22:28 +0100

    Update Sum ALL the arrays!.js

--- Sum ALL the arrays!.js
+++ Sum ALL the arrays!.js
@@ -8,8 +8,13 @@ Ignore all other types of values.
 
  */
 //My solution:
+//1.
 function arraySum(arr) {
     // sum ALL the things!
     return  arr.reduce((a, b) => a + (typeof(b) == "number" ? b : (typeof(b) == "object" ? arraySum(b) : 0)), 0);
 
+}
+//2.
+function arraySum(arr) {
+  return arr.reduce((n, x) => n + (Array.isArray(x) ? arraySum(x) : isNaN(x) ? 0 : x), 0)
 }
